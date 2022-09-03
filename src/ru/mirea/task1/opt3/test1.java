f7dde2f353f572d235529ae8c82b59cf4faabe4e
lexCooper1121 <alexbo007@yandex.ru>
2022-09-03 11:42:04 +0300

    Задание 1 из задания 1

---
+++ src/ru/mirea/task1/opt3/test1.java
@@ -0,0 +1,12 @@
+package ru.mirea.task1.opt3;
+
+public class test1 {
+    public static void main(String[] args){
+        int myArr[] = {1,2,3,4};
+        int sum = 0;
+        for (int i=0; i<myArr.length; i++){
+            sum=sum+myArr[i];
+        }
+        System.out.print(sum);
+    }
+}
